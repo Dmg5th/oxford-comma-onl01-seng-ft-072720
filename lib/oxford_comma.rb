d0489ff16a509d1c00523a230645095ae6bbb784
Daniel Gould <dmgould310@gmail.com>
2020-07-30 11:08:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ def oxford_comma(array)
     "#{array[0]}"
   elsif array.length == 2
   array.join(" and ")
-  elsif array.length == 3
-  array.join(", and ")
+  else 
+  array.join(", ")
+  array.push (" and ")
   end 
 end
